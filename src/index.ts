26e234f4fd727dead82134df57544fe0402e78fb
xhayper <46065349+xhayper@users.noreply.github.com>
2022-12-09 11:08:06 +0700

    chore: code clean up

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import fastify, { type RouteShorthandOptions } from "fastify";
-import { getCountImage, themeList } from "./utils/themify";
-import { PrismaClient } from "@prisma/client";
-import path from "path";
+import fastify, { type RouteShorthandOptions } from 'fastify';
+import { getCountImage, themeList } from './utils/themify';
+import { PrismaClient } from '@prisma/client';
+import path from 'node:path';
 
 (async () => {
   const prisma = new PrismaClient();
@@ -9,97 +9,93 @@ import path from "path";
   const server = fastify({
     logger: {
       transport: {
-        target: "@fastify/one-line-logger",
-      },
-    },
+        target: '@fastify/one-line-logger'
+      }
+    }
   });
 
-  await server.register(import("@fastify/static"), {
-    root: path.join(__dirname, "../assets"),
+  await server.register(import('@fastify/static'), {
+    root: path.join(__dirname, '../assets')
   });
 
-  await server.register(import("@fastify/compress"));
+  await server.register(import('@fastify/compress'));
 
   const customizationOption: RouteShorthandOptions = {
     schema: {
       querystring: {
-        type: "object",
+        type: 'object',
         properties: {
           theme: {
-            type: "string",
-            enum: Object.keys(themeList),
+            type: 'string',
+            enum: Object.keys(themeList)
           },
           length: {
-            type: "number",
+            type: 'number',
             minimum: 1,
-            maximum: 12,
+            maximum: 12
           },
           pixelated: {
-            type: "boolean",
-          },
+            type: 'boolean'
+          }
         },
-        additionalProperties: false,
-      },
-    },
+        additionalProperties: false
+      }
+    }
   };
 
-  server.get("/count/:identifier", customizationOption, async (req, res) => {
+  server.get('/count/:identifier', customizationOption, async (req, res) => {
     const { identifier } = req.params as any;
     let { theme, length, pixelated } = req.query as any;
 
-    theme = theme ?? "moebooru";
-    length = parseInt(length ?? "7");
+    theme = theme ?? 'moebooru';
+    length = parseInt(length ?? '7');
 
     if (!identifier || identifier.length > 256) {
       res.code(400);
       return {
         statusCode: 400,
-        error: "Bad Request",
-        message: "Invalid identifier",
+        error: 'Bad Request',
+        message: 'Invalid identifier'
       };
     }
 
     let count: string | number = 0;
-    if (identifier !== "demo") {
-      res.header(
-        "cache-control",
-        "max-age=0, no-cache, no-store, must-revalidate"
-      );
+    if (identifier !== 'demo') {
+      res.header('cache-control', 'max-age=0, no-cache, no-store, must-revalidate');
 
       let CountData = await prisma.count.findFirst({
         where: {
-          identifier,
-        },
+          identifier
+        }
       });
 
       if (!CountData)
         CountData = await prisma.count.create({
           data: {
-            identifier,
-          },
+            identifier
+          }
         });
 
-      if (Number.MAX_SAFE_INTEGER > CountData.count)
-        count = CountData.count + 1;
+      if (Number.MAX_SAFE_INTEGER > CountData.count) count = CountData.count + 1;
 
       if (CountData.count !== count)
         await prisma.count.update({
           where: {
-            identifier,
+            identifier
           },
           data: {
-            count,
-          },
+            count
+          }
         });
     } else {
-      count = "0123456789";
+      count = '0123456789';
     }
 
-    res.header("content-type", "image/svg+xml");
+    res.header('content-type', 'image/svg+xml');
     return getCountImage({ count, theme, length, pixelated });
   });
 
-  server.get("/number/:amount", customizationOption, async (req, res) => {
+  server.get('/number/:amount', customizationOption, async (req, res) => {
     let { amount } = req.params as any;
     let { theme, length, pixelated } = req.query as any;
 
@@ -107,34 +103,34 @@ import path from "path";
       res.code(400);
       return {
         statusCode: 400,
-        error: "Bad Request",
-        message: "number length must be <= 32",
+        error: 'Bad Request',
+        message: 'number length must be <= 32'
       };
     }
 
-    theme = theme ?? "moebooru";
-    length = parseInt(length ?? "7");
+    theme = theme ?? 'moebooru';
+    length = parseInt(length ?? '7');
     amount = parseInt(amount);
 
     if (!amount || 0 > amount) {
       res.code(400);
       return {
         statusCode: 400,
-        error: "Bad Request",
-        message: "number must be a valid positive integer",
+        error: 'Bad Request',
+        message: 'number must be a valid positive integer'
       };
     }
 
-    res.header("content-type", "image/svg+xml");
+    res.header('content-type', 'image/svg+xml');
     return getCountImage({ count: amount, theme, length, pixelated });
   });
 
-  server.get("/heart-beat", () => {
-    return "alive";
+  server.get('/heart-beat', () => {
+    return 'alive';
   });
 
   server.listen({
-    host: process.env.HOST ?? "0.0.0.0",
-    port: parseInt(process.env.PORT ?? "3000"),
+    host: process.env.HOST ?? '0.0.0.0',
+    port: parseInt(process.env.PORT ?? '3000')
   });
 })();
